4a9d09dceb5c6d3462ac8dc30a93a887da67a146
daphnemesquita2 <d.j.gomes.de.mesquita@student.tue.nl>
2026-03-17 11:08:15 +0100

    set up

--- Assignment 2/Group07_1BM110_A2_part2.py
+++ Assignment 2/Group07_1BM110_A2_part2.py
@@ -3,3 +3,30 @@
 #### PART 2: Training the Agent ##########
 
 ##########################################
+
+
+import numpy as np
+from knapsack_env import BoundedKnapsackEnv
+from stable_baselines3 import DQN, PPO
+from sb3_contrib import MaskablePPO
+from sb3_contrib.common.wrappers import ActionMasker
+
+
+'''Set up using AI'''
+
+# Set seed
+seed = 2024
+np.random.seed(seed)
+
+# Create environment
+env = BoundedKnapsackEnv(n_items=200, max_weight=200)
+
+# For Part 3 (masked version):
+def mask_fn(env):
+    return env.get_mask()
+
+env = BoundedKnapsackEnv(n_items=200, max_weight=200, mask=True)
+env = ActionMasker(env, mask_fn)
+
+# Create model (seed set here too)
+model = DQN("MlpPolicy", env, seed=seed)
--- Assignment 2/Group07_1BM110_A2_part3.py
+++ Assignment 2/Group07_1BM110_A2_part3.py
@@ -3,3 +3,13 @@
 #### PART 3: Training the Agent with Invalid Action Masking###########
 
 ######################################################################
+
+
+import numpy as np
+from knapsack_env import BoundedKnapsackEnv
+from stable_baselines3 import DQN, PPO
+from sb3_contrib import MaskablePPO
+from sb3_contrib.common.wrappers import ActionMasker
+
+
+
